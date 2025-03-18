1139eaa2215f174b4b371d67736bbdd5ebd147fa
José Alejandro Campos Portillo <51547808+AlejandroCaMattC@users.noreply.github.com>
2025-03-18 17:54:57 -0600

    Mi primer archivo Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AlejandroCaMattC.py
@@ -0,0 +1,11 @@
+# https://www.python.org/
+
+"""This is another way to write a comment in Python"""
+
+# Variables
+# Variables are containers for storing data values.
+name = "Alejandro"
+
+# Constants
+# Constants are fixed values that a program may not alter during its execution.
+FIRST_CONSTANT = "Hello, World!"
