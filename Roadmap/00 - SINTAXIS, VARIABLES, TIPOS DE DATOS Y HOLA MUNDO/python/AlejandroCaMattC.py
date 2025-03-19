4477890b828a8fea1c651e3eb66c6956fa37a650
José Alejandro Campos Portillo <51547808+AlejandroCaMattC@users.noreply.github.com>
2025-03-18 20:16:29 -0600

    Second commit

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AlejandroCaMattC.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AlejandroCaMattC.py
@@ -1,11 +1,18 @@
 # https://www.python.org/
 
-"""This is another way to write a comment in Python"""
+# This is a comment
+# Comments are used to explain code when the Python interpreter ignores them.
+"""This is another way to write a comment in Python for multiple lines."""
+'''This is another way to write a comment in Python for multiple lines.'''
 
 # Variables
 # Variables are containers for storing data values.
-name = "Alejandro"
+name = "Alejandro"  # This is a string
+age = 20  # This is an integer
+height = 1.75  # This is a float (decimal)
+is_student = True  # This is a boolean
+
 
 # Constants
 # Constants are fixed values that a program may not alter during its execution.
-FIRST_CONSTANT = "Hello, World!"
+FIRST_CONSTANT = "Hello, World!"  # This is how a constant is declared in Python
