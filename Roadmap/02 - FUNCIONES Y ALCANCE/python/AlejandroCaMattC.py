283239fbc76fae4a472a9e359fd90c3d93f6e6b8
José Alejandro Campos Portillo <51547808+AlejandroCaMattC@users.noreply.github.com>
2025-03-20 20:46:40 -0600

    032025-FirstCommit

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/AlejandroCaMattC.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/AlejandroCaMattC.py
@@ -72,3 +72,94 @@ print(multiple_return_greet())
 greet, name = multiple_return_greet()
 print(greet)
 print(name)
+
+# Function with variable number of arguments
+print("\nFunction with variable number of arguments")
+
+
+def var_args_greet(*names):
+    for name in names:
+        print(f"Hello, {name}!")
+
+
+var_args_greet("Python", "Java", "C++")
+
+# Function with variable number of keyword arguments
+print("\nFunction with variable number of keyword arguments")
+
+
+def var_kwargs_greet(**names):
+    for key, value in names.items():
+        print(f"Hello, {value} ({key})!")
+
+
+var_kwargs_greet(
+    language="Python",
+    name="Alejo",
+    alias="Alejandro",
+    age=36
+)
+
+
+"""Functions inside functions"""
+print("\nFunctions inside functions")
+
+
+def outer_function():
+    def inner_function():
+        print("Inner function")
+    inner_function()
+
+
+outer_function()
+
+"""Built-in functions"""
+print("\nBuilt-in functions")
+
+
+print(len("Alejandro"))
+print(type(36))
+print(type("Alejandro"))
+print(str(36))
+print(int("36"))
+print("Alejandro".upper())
+
+
+"""Local and global scope"""
+print("\nLocal and global scope")
+
+global_variable = "Python"
+
+print(global_variable)
+
+
+def hello_python():
+    local_variable = "Java"
+    print(f"Hello, {local_variable}, {global_variable}!")
+
+
+print(global_variable)
+# print(local_variable)  # Cannot access local variable
+hello_python()
+
+
+"""Exercise"""
+print("\nExercise")
+
+
+def print_numbers(text_1, text_2) -> int:
+    count = 0
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print(f"{text_1} {text_2}")
+        elif number % 3 == 0:
+            print(text_1)
+        elif number % 5 == 0:
+            print(text_2)
+        else:
+            print(number)
+            count += 1
+    return count
+
+
+print(print_numbers("Hello", "Python"))
