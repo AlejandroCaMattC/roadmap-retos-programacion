394527be730a0fb79d36f7b175cbefed29664853
José Alejandro Campos Portillo <51547808+AlejandroCaMattC@users.noreply.github.com>
2025-04-08 18:29:34 -0600

    04082025-FirstCommit

---
+++ Roadmap/14 - FECHAS/python/AlejandroCaMattC.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+
+
+"""Dates"""
+
+now = datetime.now()
+print("Current date and time:", now)
+print("Current date:", now.date())
+print("Current time:", now.time())
+print("Current year:", now.year)
+print("Current month:", now.month)
+print("Current day:", now.day)
+print("Current hour:", now.hour)
+print("Current minute:", now.minute)
+print("Current second:", now.second)
