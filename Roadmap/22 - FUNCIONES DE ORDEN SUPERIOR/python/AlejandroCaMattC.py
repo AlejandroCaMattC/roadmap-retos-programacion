66999e112fa972079d87d0893d368c16962083a8
José Alejandro Campos Portillo <51547808+AlejandroCaMattC@users.noreply.github.com>
2025-05-12 21:01:32 -0600

    05122025-FirstCommit

--- Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/python/AlejandroCaMattC.py
+++ Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/python/AlejandroCaMattC.py
@@ -1,15 +1,15 @@
 """ Higher Order Functions"""
 
-# Higher Order Functions are functions that can take
-# other functions as arguments or return them as results.
-# They are a key concept in functional programming and allow for more abstract
-# and flexible code.
-# In Python, functions are first-class citizens, meaning they can be passed
-# around as arguments, returned from other functions, and assigned to variables.
-# This allows for the creation of higher-order functions that can operate on other functions.
-
+# Higher Order Functions are functions that can take other functions as arguments
+# or return them as results. They are a key concept in functional programming and
+# allow for more abstract and flexible code. In Python, functions are first-class
+# citizens, meaning they can be passed around as arguments, returned from other
+# functions, and assigned to variables. This allows for the creation of higher-
+# order functions that can operate on other functions.
 
 from functools import reduce
+
+from datetime import datetime
 
 
 def apply_func(func, x):
@@ -20,6 +20,7 @@ print(apply_func(len, "Hello World"))  # 11
 
 
 # Higher-order functions can also return other functions.
+
 
 def apply_multiplier(n):
     def multiplier(x):
@@ -57,7 +58,6 @@ def is_even(n):
 
 print(list(filter(is_even, numbers)))  # [2]
 
-
 # sorted()
 
 print(sorted(numbers))  # [1, 2, 3, 4, 5]
@@ -66,9 +66,69 @@ print(sorted(numbers, key=lambda x: -x))  # [5, 4, 3, 2, 1]
 
 # reduce()
 
+# Renamed the custom 'sum' function to 'add' to avoid conflict with the built-in 'sum'.
 
-def sum(x, y):
+
+def add(x, y):
     return x + y
 
 
-print(reduce(sum, numbers))
+# Updated the reduce call to use 'add' instead of 'sum'.
+print(reduce(add, numbers))
+
+"""Exercise"""
+
+# Students list with names, birthdays and a lists of grades
+students = [
+    {
+        "name": "Alejandro",
+        "birthday": "1985-17-06",
+        "grades": [10, 9, 8, 3, 6]
+    },
+    {
+        "name": "Matias",
+        "birthday": "1995-01-07",
+        "grades": [10, 4, 8, 7, 6]
+    },
+    {
+        "name": "Cecilia",
+        "birthday": "2001-05-08",
+        "grades": [10, 9, 8, 7, 10]
+    }
+]
+
+# Calculate the average of the grades of each student
+
+
+def calculate_average(grades):
+    return sum(grades) / len(grades)
+
+
+print(
+    list(
+        map(
+            lambda student: {
+                "name": student["name"],
+                "average": calculate_average(student["grades"])
+            },
+            students
+        )
+    )
+)
+
+# Print a list of the students with an average greater than 9
+
+
+print(
+    list(map(lambda student: student["name"], filter(
+        lambda student: calculate_average(student["grades"]) > 7,
+        students
+    )))
+)
+
+# Print the list of students sorted by the youngest to the oldest
+
+
+print(sorted(students, key=lambda student: datetime.strptime(
+    student["birthday"], "%Y-%d-%m"), reverse=True
+))
